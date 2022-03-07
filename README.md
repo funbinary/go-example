0a4a34b65af868398158edbd90f93bcdec88d078
huzb <huzhibin@beyondinfo.com.cn>
2022-03-07 20:44:25 +0800

    feat: 说明和测试程序

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # go-example
 go编程示例
+
+```go
+ - standard 标准库示例
+ - go-redis redis库使用示例
+ - testing 测试库使用示例
+ - viper 配置解析库viper使用示例
+```
+
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"index/suffixarray"
+)
+
+func main() {
+	index := suffixarray.New([]byte("banana"))
+	offsets := index.Lookup([]byte("ana"), -1)
+	fmt.Println(offsets)
+	for _, off := range offsets {
+		fmt.Println(off)
+	}
+
+}
